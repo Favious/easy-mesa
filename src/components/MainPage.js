12671a9836fa465351af36cfb600a3798953c882
Favious <faviopaco@gmail.com>
2021-12-28 15:40:02 -0400

    change fontawesome call

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -4,6 +4,7 @@ import {SearchBar} from "./SearchBar";
 
 export const MainPage = (props) => {
     return(<div className="mr-robot">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
         <div className="evilcorp">
             <div className="fsociety-mask"></div>
             <div className="title">EASY_MESA</div>
